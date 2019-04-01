ac7dc431b0459355e0d8392b194ccdefb0ebc53e
david.henriquez <david.henriquez@klaviyo.com>
2019-04-01 18:47:20 -0400

    Adds Cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from "axios"
+import Card from "react-bootstrap/Card"
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
+
   componentDidMount() {
     axios.get('https://reqres.in/api/users?page=2')
       .then(response => {
         const { data: { data } } = response
-        this.setState({data: data})
+        this.setState({ data: data })
       })
       .catch(err => {
         this.setState({data: ["error retrieving names"]})
       })
   }
 
-  handleClick = () => {
+  handleClick = (idx) => {
+    const currentState = [...this.state.data]
+    const newState = currentState.filter( (item, index) => {
+      if (index !== idx) {
+        return item
+      }
+    })
+
     this.setState({
-      message: "Clicked"
+      data: newState
     })
   }
 
   render() {
     return(
-      <div>{this.state.data && this.state.data.map( (person, index) =>
-        <div key={index}>{person.first_name}</div>
+      <div>
+        {this.state.data && this.state.data.map( (person, index) => {
+        return (
+          // <div>{person.first_name}</div>
+          <div key={index}>
+            <Card style={{ width: '18rem' }}>
+              <div style={{ marginLeft: "80%", marginTop: 5, fontSize: 30}} onClick={ e => this.handleClick(index)}>X</div>
+              <Card.Body>
+                <img src={person.avatar}/>
+                <Card.Title>{person.first_name} {person.last_name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+        )
+      }
       )}
       </div>
     )
