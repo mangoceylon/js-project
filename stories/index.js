8fe3ffa80d3977a57101b80c918d2ae6f6d9e0d8
david.henriquez <david.henriquez@klaviyo.com>
2019-04-10 19:40:56 -0400

    Adds storybook and material design

---
+++ stories/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from '@storybook/react/demo';
+import Card from "react-bootstrap/Card"
+import 'bootstrap/dist/css/bootstrap.css';
+import RecipeReviewCard from "../src/mDC"
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button>Hello Button</Button>
+  ))
+  .add('with emoji', () => (
+    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ));
+
+storiesOf('Card', module)
+  .add('with details', () => (
+    <Card style={{ width: '18rem' }}>
+      <div style={{ marginLeft: "80%", marginTop: 5, fontSize: 30}}>X</div>
+      <Card.Body>
+        <img alt={"avatar"}src="https://vignette.wikia.nocookie.net/athfwiki/images/e/eb/Ignignokt.png/revision/latest?cb=20080316001408.jpg"/>
+        <Card.Title>Ignignokt</Card.Title>
+      </Card.Body>
+    </Card>
+  ))
+  .add('default', () => (
+    <Card>
+    <p>Hello world.</p>
+    </Card>
+  ))
+
+storiesOf('RecipeReviewCard', module)
+  .add('default', () => (
+    <RecipeReviewCard />
+  ))
